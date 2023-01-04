346dd995c441a81ebc02ef03beea662751063db4
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-03 21:44:32 -0300

    :sparkles:	feat: controle do livro

---
+++ livraria/src/main/java/br/com/livraria/LivroBean.java
@@ -0,0 +1,18 @@
+package br.com.livraria;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+    
+    private Livro livro = new Livro();
+    
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void gravar() {
+        System.out.println("Gravando: "+this.livro.getTitulo());
+    }
+    
+}
