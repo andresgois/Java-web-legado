4541e918bc2013b210d24fd57df73899098fc9c7
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-05 22:12:07 -0300

    :sparkles: feat: busca autores

--- livraria/src/main/java/br/com/livraria/bean/LivroBean.java
+++ livraria/src/main/java/br/com/livraria/bean/LivroBean.java
@@ -1,16 +1,35 @@
 package br.com.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
 import br.com.livraria.dao.DAO;
+import br.com.livraria.modelo.Autor;
 import br.com.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
     
     private Livro livro = new Livro();
+    private Integer autorId;
     
     public Livro getLivro() {
         return livro;
+    }
+    
+    public List<Autor> getAutores(){
+        List<Autor> autores = new DAO<Autor>(Autor.class).listaTodos();
+        return autores;
+    }
+    
+    public void gravarAutor() {
+        System.out.println("AutorId = "+this.autorId);
+        Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+        this.livro.adicionaAutor(autor);
+        System.out.println("============== "+autor.getNome());
     }
 
     public void gravar() {
@@ -21,6 +40,14 @@ public class LivroBean {
         }
         
         new DAO<Livro>(Livro.class).adiciona(this.livro);
+    }
+
+    public Integer getAutorId() {
+        return autorId;
+    }
+
+    public void setAutorId(Integer autorId) {
+        this.autorId = autorId;
     }
     
 }
