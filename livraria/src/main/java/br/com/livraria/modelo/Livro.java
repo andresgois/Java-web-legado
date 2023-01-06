3b5b359043f24cb313941af3cdb4746a11af7cbb
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-06 19:02:23 -0300

    :sparkles:	feat: valor automatico para id

--- livraria/src/main/java/br/com/livraria/modelo/Livro.java
+++ livraria/src/main/java/br/com/livraria/modelo/Livro.java
@@ -2,14 +2,17 @@ package br.com.livraria.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 @Entity
 public class Livro {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String isbn;
@@ -57,11 +60,11 @@ public class Livro {
     public void setDataLancamento(String dataLancamento) {
         this.dataLancamento = dataLancamento;
     }
-
+    
     public List<Autor> getAutores() {
         return autores;
     }
-
+    
     public void setAutores(List<Autor> autores) {
         this.autores = autores;
     }
