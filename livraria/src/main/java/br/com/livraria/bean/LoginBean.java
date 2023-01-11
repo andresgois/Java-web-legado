12d5f408ebbc39f5561500726ba7c2282515f545
Francisco Andre De Sousa Gois <andresousalee@gmail.com>
2023-01-11 18:37:59 -0300

    :sparkles:	feat: tratamento do usuario logado

--- livraria/src/main/java/br/com/livraria/bean/LoginBean.java
+++ livraria/src/main/java/br/com/livraria/bean/LoginBean.java
@@ -1,7 +1,9 @@
 package br.com.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.livraria.dao.UsuarioDAO;
 import br.com.livraria.modelo.Usuario;
@@ -18,13 +20,24 @@ public class LoginBean {
     
     public String efetuarLogin() {
         System.out.println("fazendo login do usuário: "+this.usuario.getEmail());
+        FacesContext context = FacesContext.getCurrentInstance();
         
         boolean existe = new UsuarioDAO().existe(this.usuario);
         if(existe ) {
+            context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
             return "livro?faces-redirect=true";
         }
-        
-        return null;
+        //context.addMessage("login:email", new FacesMessage("Usuário não encontrado"));
+        // messagem existiram por 2 requisições
+        context.getExternalContext().getFlash().setKeepMessages(true);
+        context.addMessage(null , new FacesMessage("Usuário não encontrado"));
+        return "login?faces-redirect=true";
+    }
+    
+    public String deslogar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().remove("usuarioLogado");
+        return "login?faces-redirect=true"; 
     }
     
 }
